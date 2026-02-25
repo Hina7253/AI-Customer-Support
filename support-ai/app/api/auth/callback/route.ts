7161c07ec81ea7120a91b1ff6da1c5ff495efed2
Hina <hinakumari.725394@gmail.com>
2026-02-25 10:27:27 +0530

    add response

---
+++ support-ai/app/api/auth/callback/route.ts
@@ -0,0 +1,13 @@
+import { scalekit } from "@/lib/scalekit";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req:NextRequest) {
+    const{searchParams} = new URL(req.url)
+    const code = searchParams.get("code")
+    const redirectUri = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`;
+ if(!code){
+    return NextResponse.json({message:"code is not found"}, {status:400})
+ }
+ const session = scalekit.authenticateWithCode(code,redirectUri)
+
+}
